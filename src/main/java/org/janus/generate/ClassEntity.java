53e3ba6f397b3a2b3b740cd0034e714b531b2878
Jorge Nascimento <jnascimento.gcc@gmail.com>
2023-06-21 21:53:38 +0100

    Setup

--- src/main/java/org/janus/generate/ClassEntity.java
+++ src/main/java/org/janus/generate/ClassEntity.java
@@ -1,8 +1,12 @@
 package org.janus.generate;
 
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeSpec;
+import com.squareup.javapoet.TypeVariableName;
 import jakarta.persistence.Entity;
 import org.apache.commons.text.CaseUtils;
+import org.springframework.data.repository.CrudRepository;
 
 import javax.lang.model.element.Modifier;
 
@@ -10,9 +14,12 @@ public class ClassEntity {
 
     public static TypeSpec generate(String tableName) {
 
+        TypeVariableName masterEntity = TypeVariableName.get("MasterEntity");
+
         return TypeSpec.classBuilder(CaseUtils.toCamelCase(tableName, true, '_') + "Entity")
                 .addAnnotation(Entity.class)
                 .addAnnotation(AnnotationTable.generate(tableName))
+                .addSuperinterface(ParameterizedTypeName.get(ClassName.get(MasterEntity.class)))
 
                 .addField(FieldSimple.generate())
 
