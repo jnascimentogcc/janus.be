5c24a3390baed3e044fcca3608639ebd566e4ff3
Jorge Nascimento <jnascimento.gcc@gmail.com>
2023-06-22 22:54:36 +0100

    Persistence Level

--- src/main/java/org/janus/generate/persistence/clazz/ClassEntity.java
+++ src/main/java/org/janus/generate/persistence/clazz/ClassEntity.java
@@ -1,6 +1,7 @@
 package org.janus.generate.persistence.clazz;
 
 import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
 import jakarta.persistence.Entity;
 import org.apache.commons.text.CaseUtils;
@@ -20,7 +21,8 @@ public class ClassEntity {
 
     public static TypeSpec generate(String tableName, List<ColumnSimpleSpec> listColumn, List<ColumnManyToOneSpec> listColumnManyToOne, List<ColumnOneToManySpec> listColumnOneToMany) {
 
-        TypeSpec.Builder classEntityBuilder = TypeSpec.classBuilder(CaseUtils.toCamelCase(tableName, true, '_') + "Entity")
+        String entityName = CaseUtils.toCamelCase(tableName, true, '_') + "Entity";
+        TypeSpec.Builder classEntityBuilder = TypeSpec.classBuilder(entityName)
                 .addAnnotation(Entity.class)
                 .addAnnotation(AnnotationTable.generate(tableName))
                 .superclass(ClassName.get("com.kadipe.helper", "MasterEntity"))
@@ -40,6 +42,26 @@ public class ClassEntity {
                     .addMethod(GetMethodOneToMany.generate(item))
                     .addMethod(SetMethodOneTOMany.generate(item));
         });
+
+        MethodSpec equals = MethodSpec.methodBuilder("equals")
+                .addModifiers(Modifier.PUBLIC)
+                .addAnnotation(Override.class)
+                .returns(boolean.class)
+                .addParameter(Object.class, "o")
+                .addStatement("if (this == o) return true")
+                .addStatement("if (o == null || getClass() != o.getClass()) return false")
+                .addStatement("return new EqualsBuilder().appendSuper(super.equals(o)).isEquals()")
+                .build();
+
+        MethodSpec hashCode = MethodSpec.methodBuilder("hashCode")
+                .addModifiers(Modifier.PUBLIC)
+                .addAnnotation(Override.class)
+                .returns(int.class)
+                .addStatement("return new HashCodeBuilder(17, 37).appendSuper(super.hashCode()).toHashCode()")
+                .build();
+
+        classEntityBuilder.addMethod(equals)
+                .addMethod(hashCode);
 
         return classEntityBuilder.build();
     }
