2fb92f01ee10e768473fb1f19dcb0bc848feb09e
Jorge Nascimento <jnascimento.gcc@gmail.com>
2023-07-09 01:32:48 -0400

    Service

--- src/main/java/org/janus/generate/persistence/clazz/ClassService.java
+++ src/main/java/org/janus/generate/persistence/clazz/ClassService.java
@@ -1,26 +1,33 @@
 package org.janus.generate.persistence.clazz;
 
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.TypeSpec;
-import jakarta.persistence.Entity;
 import jakarta.transaction.Transactional;
 import org.apache.commons.text.CaseUtils;
-import org.janus.db.ColumnManyToOneSpec;
-import org.janus.db.ColumnOneToManySpec;
-import org.janus.db.ColumnSimpleSpec;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.lang.model.element.Modifier;
-import java.util.List;
 
 public class ClassService {
 
     public static TypeSpec generate(String serviceName) {
 
-        String dtoName = CaseUtils.toCamelCase(serviceName, true, ' ') + "Service";
-        TypeSpec.Builder classDTOBuilder = TypeSpec.classBuilder(dtoName)
+        String clazzName = CaseUtils.toCamelCase(serviceName, true, ' ') + "Service";
+
+        FieldSpec fieldLogger = FieldSpec.builder(Logger.class, "LOGGER")
+                .addModifiers(Modifier.PRIVATE, Modifier.FINAL, Modifier.STATIC)
+                .initializer("$T.$L", ClassName.get(LoggerFactory.class), "getLogger(" + clazzName + ".class)")
+                .build();
+
+        TypeSpec.Builder classDTOBuilder = TypeSpec.classBuilder(clazzName)
                 .addAnnotation(Service.class)
                 .addAnnotation(Transactional.class)
-                .addModifiers(Modifier.PUBLIC);
+                .addModifiers(Modifier.PUBLIC)
+                .addField(fieldLogger)
+                ;
 
         return classDTOBuilder.build();
     }
